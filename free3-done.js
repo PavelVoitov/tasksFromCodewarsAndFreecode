9c6f311d1b39c75b23c6e198a14fd9fb0c3029d0
PavelVoitov <voitov.pavel@yandex.ru>
2022-08-09 18:21:42 +0300

    new task done

---
+++ free3-done.js
@@ -0,0 +1,11 @@
+function rangeOfNumbers(startNum, endNum) {
+    
+    return startNum === endNum ? [startNum]
+    :   startNum > endNum ? [] 
+    :   rangeOfNumbers(startNum, endNum - 1).concat(endNum);
+    
+  };
+
+  console.log(rangeOfNumbers(1, 6));
+
+ 
