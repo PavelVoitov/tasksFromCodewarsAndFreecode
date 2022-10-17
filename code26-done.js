9ef37d9c8c04e7dbd00d1a5c5dc17791198dc1ff
PavelVoitov <voitov.pavel@yandex.ru>
2022-10-17 19:08:59 +0300

    26 27

---
+++ code26-done.js
@@ -0,0 +1,16 @@
+function countSheeps(arrayOfSheep) {
+    let count = 0
+    for (let i = 0; i < arrayOfSheep.length; i++) {
+      if (arrayOfSheep[i] === true) {
+        count += 1
+      }
+    }
+   return count
+  }
+
+//   console.log(countSheeps([true,  true,  true,  false,
+//     true,  true,  true,  true ,
+//     true,  false, true,  false,
+//     true,  false, false, true ,
+//     true,  true,  true,  true ,
+//     false, false, true,  true]))
---
+++ code27-done.js
@@ -0,0 +1,14 @@
+function giveMeFive(obj){
+    let five = []
+    for (var key in obj){
+     if (key.length === 5 ) {
+        five.push(key)
+     }
+     if (obj[key].length === 5) {
+        five.push(obj[key])
+     }
+   }
+  return five;
+ }
+
+//  console.log(giveMeFive({Our:"earth",is:"a",beautyful:"world"}))
